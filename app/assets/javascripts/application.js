728908e1aac09650c22540ffbb315fab855b0545
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-12-17 20:11:33 -0600

    Adding bacbone and underscore.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,4 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require jquery.cookie.min.js
+//= require underscore-min.js
 //= require_tree .
